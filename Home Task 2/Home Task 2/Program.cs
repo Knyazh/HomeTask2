031685f6b58d8e66b838ec9f805104d4c0c46033
Hp <qnyazface@maul.ru>
2023-03-04 06:25:13 -0800

    Final

---
+++ Home Task 2/Home Task 2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+double overageBudget = 20000000000;
+Console.WriteLine("Budce :" + overageBudget);
+
+int armyFee = 20;
+int techFee = 10;
+
+
+double totaArmyfee = overageBudget * armyFee / 100;
+Console.WriteLine("Mudafie Nazirliyine :" + totaArmyfee);
+
+double totalTechfee = overageBudget * techFee / 100;
+Console.WriteLine("Yuksek Texnologiya Nazirliyine :" + totalTechfee);
+
+double total = overageBudget - totalTechfee -  totalTechfee;
+Console.WriteLine("Yerde Qalan Budce :" + total);
